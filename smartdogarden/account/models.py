c0f01c15afce92ba670a70c6261b38535bff1f77
Liron Gabai <lirong6@gmail.com>
2020-04-06 13:44:31 +0300

    PM2020T8-1 add a README file to the project.

---
+++ smartdogarden/account/models.py
@@ -0,0 +1,81 @@
+import uuid
+
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from phone_field import PhoneField
+
+# Create your models here.
+
+
+class MyAccountManager(BaseUserManager):
+    def create_user(self, user_id, email, username, password=None,):
+        if not email:
+            raise ValueError("Users must have an email address")
+        if not username:
+            raise ValueError("Users must have an username")
+
+        user = self.model(
+                user_id=user_id,
+                email=self.normalize_email(email),
+                username=username,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, user_id, email, username, password):
+        user = self.create_user(
+                user_id=user_id,
+                email=self.normalize_email(email),
+                password=password,
+                username=username,
+        )
+        user.is_admin = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class Account(AbstractBaseUser):
+    email = models.EmailField(verbose_name="email", max_length=60, unique=False)
+    user_id = models.IntegerField(unique=True)
+    username = models.CharField(max_length=30, unique=True)
+    password = models.CharField(max_length=150, unique=False)
+    first_name = models.CharField(max_length=30, unique=False)
+    last_name = models.CharField(max_length=30, unique=False)
+    phone_number = PhoneField(blank=True)
+    city = models.CharField(max_length=30, unique=False)
+    street = models.CharField(max_length=30, unique=False)
+    app_number = models.IntegerField(null=True, unique=False)
+    zip = models.IntegerField(null=True, unique=False)
+    date_joined = models.DateTimeField(verbose_name='date joined', auto_now_add=True)
+    last_login = models.DateTimeField(verbose_name='last login', auto_now_add=True)
+    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_dog_owner = models.BooleanField(default=False)
+    is_dog_sitter = models.BooleanField(default=False)
+    is_dog_garden_admin = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = [
+         'email', 'user_id'
+    ]
+
+    objects = MyAccountManager()
+
+    def __str__(self):
+        return self.username
+
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+
+    #@staticmethod
+    def has_module_perms(self, app_label):
+        return True
+
+    def get_id(self):
+        return self.user_id
