8030319f90f79b7727d98bef46eee34de5cdfde1
orel dayan <oreld321@gmail.com>
2020-04-06 15:28:05 +0300

    PM2020T8-3 add a README file to the project.

---
+++ smartdogarden/users/forms.py
@@ -0,0 +1,34 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from account.models import Account
+
+
+class RegistrationFormDogOwner(UserCreationForm):
+    email = forms.EmailField()
+    user_id = Account.user_id
+
+    class Meta:
+        model = Account
+        fields = [
+            'username',
+            'email',
+            'password1',
+            'password2',
+            'first_name',
+            'last_name',
+            'user_id',
+            'city',
+            'street',
+            'app_number',
+            'zip',
+            'phone_number',
+        ]
+
+    def save(self, commit=True):
+        user = super(RegistrationFormDogOwner, self).save(commit=False)
+        user.is_dog_owner = True
+        if commit:
+            user.save()
+
+            return user
