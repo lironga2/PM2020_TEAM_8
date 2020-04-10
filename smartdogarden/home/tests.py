5c07250f31645016314869186bd7c1b91c119eb0
ShirEliyahu <44493018+ShirEliyahu@users.noreply.github.com>
2020-04-10 17:36:49 +0300

    tests and update the jenkinsfile to run the tests

--- smartdogarden/home/tests.py
+++ smartdogarden/home/tests.py
@@ -1,3 +1,35 @@
+import unittest
+from django.test import Client
 from django.test import TestCase
 
 # Create your tests here.
+
+from django.contrib.auth import get_user_model
+# from django.contrib.auth import get_user_model
+from django.contrib.auth import SESSION_KEY
+from django.test import TestCase
+
+
+class MyTestCase(unittest.TestCase):
+    # def LogInTest(TestCase):
+    c = Client()
+
+    def test_insert_to_DB(self):
+
+        self.credentials = {
+            'user_id': '555',
+            'email': 'super@super.super',
+            'username': 'super',
+            'password': 'l123123123'}
+        with self.assertRaisesRegex(Exception, "Duplicate entry"):
+            get_user_model().objects.create_user(**self.credentials)
+        # get_user_model().objects.save(using=self._db)
+
+    def test_login(self):
+        c = Client()
+        login = c.post('/login/', {'username': 'super', 'password': 'l123123123'})
+        self.assertTrue(login)
+
+
+if __name__ == '__main__':
+    unittest.main()
